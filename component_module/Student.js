9986dc026895ebbdf076e3688b5266c0d24437f8
= <kimadonghyon99@gmail.com>
2023-02-24 17:49:27 +0900

    plz add delbtn click event

--- component_module/Student.js
+++ component_module/Student.js
@@ -8,7 +8,7 @@ export default class Student extends Component{
         const {Student} = this.state;
         return`
         <ul>
-            ${Student.map(stu=>`<li>${stu}</li>`).join(" ")}
+            ${Student.map((stu,index)=>`<li>${stu}<button class='delbtn' data-index='${index}'>삭제(찡긋)</button></li>`).join(" ")}
         </ul>
         <button id="add">추가</button>
         `
@@ -18,5 +18,12 @@ export default class Student extends Component{
         this.target.querySelector("#add").addEventListener('click',()=>{
             this.setState({Student:[...Student,`student${Student.length+1}`]}) //이벤트가 실행되면 setState메소드 실행
         })
+        this.target.querySelectorAll("delbtn").forEach(del => {
+            del.addEventListener('click',(e)=>{
+                const {Student} = this.state;
+                Student.filter((li,index)=>index !== e.target.value)
+                this.setState(Student.filter((li,index)=>index !== e.target.value))
+            })
+        });
     }
 }
